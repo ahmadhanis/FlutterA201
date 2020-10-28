661a864846820018b83cbcade8fd3f26fbe035f6
Ahmad Hanis <slumberjer@gmail.com>
2020-10-28 15:28:05 +0800

    new

---
+++ food_ninja/lib/loginpage.dart
@@ -0,0 +1,194 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:food_ninja/mainlayout.dart';
+import 'package:food_ninja/registeruser.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:toast/toast.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _emcontroller = TextEditingController();
+  String _email = "";
+  final TextEditingController _pscontroller = TextEditingController();
+  String _pass = "";
+  bool _isChecked = false;
+
+  @override
+  void initState() {
+    loadpref();
+    print('Init: $_email');
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+        onWillPop: _onBackPressAppBar,
+        child: Scaffold(
+            appBar: AppBar(
+              title: Text('Login'),
+            ),
+            //resizeToAvoidBottomPadding: false,
+            body: new Container(
+              padding: EdgeInsets.all(30.0),
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Image.asset(
+                      'assets/images/foodninjared.png',
+                      width: 190,
+                      height: 190,
+                    ),
+                    TextField(
+                        controller: _emcontroller,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                            labelText: 'Email', icon: Icon(Icons.email))),
+                    TextField(
+                      controller: _pscontroller,
+                      decoration: InputDecoration(
+                          labelText: 'Password', icon: Icon(Icons.lock)),
+                      obscureText: true,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    MaterialButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0)),
+                      minWidth: 300,
+                      height: 50,
+                      child: Text('Login'),
+                      color: Colors.black,
+                      textColor: Colors.white,
+                      elevation: 15,
+                      onPressed: _onPress,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Checkbox(
+                          value: _isChecked,
+                          onChanged: (bool value) {
+                            _onChange(value);
+                          },
+                        ),
+                        Text('Remember Me', style: TextStyle(fontSize: 16))
+                      ],
+                    ),
+                    GestureDetector(
+                        onTap: _onRegister,
+                        child: Text('Register New Account',
+                            style: TextStyle(fontSize: 16))),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    GestureDetector(
+                        onTap: _onForgot,
+                        child: Text('Forgot Account',
+                            style: TextStyle(fontSize: 16))),
+                  ],
+                ),
+              ),
+            )));
+  }
+
+  void _onPress() {
+    print('Press');
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => MainLayout(
+                  status: null,
+                )));
+  }
+
+  void _onRegister() {
+    print('onRegister');
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => RegisterUser()));
+  }
+
+  void _onForgot() {
+    print('Forgot');
+  }
+
+  void _onChange(bool value) {
+    setState(() {
+      _isChecked = value;
+      savepref(value);
+    });
+  }
+
+  void loadpref() async {
+    print('Inside loadpref()');
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    _email = (prefs.getString('email'));
+    _pass = (prefs.getString('pass'));
+    if (_email != null && _pass != null) {
+      _emcontroller.text = _email;
+      _pscontroller.text = _pass;
+      setState(() {
+        _isChecked = true;
+      });
+    } else {
+      print('No pref');
+      setState(() {
+        _isChecked = false;
+      });
+    }
+  }
+
+  void savepref(bool value) async {
+    print('Inside savepref');
+    _email = _emcontroller.text;
+    _pass = _pscontroller.text;
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    if (value) {
+      //true save pref
+      if (_isEmailValid(_email) || _pass.length < 5) {
+        await prefs.setString('email', _email);
+        await prefs.setString('pass', _pass);
+        print('Save pref $_email');
+        print('Save pref $_pass');
+        Toast.show("Preferences saved succesfully", context,
+            duration: Toast.LENGTH_LONG, gravity: Toast.BOTTOM);
+      } else {
+        print('No email');
+        setState(() {
+          _isChecked = false;
+        });
+        Toast.show("Invalid Preferences", context,
+            duration: Toast.LENGTH_LONG, gravity: Toast.BOTTOM);
+      }
+    } else {
+      await prefs.setString('email', '');
+      await prefs.setString('pass', '');
+      setState(() {
+        _emcontroller.text = '';
+        _pscontroller.text = '';
+        _isChecked = false;
+      });
+      print('Remove pref');
+      Toast.show("Preferences removed", context,
+          duration: Toast.LENGTH_LONG, gravity: Toast.BOTTOM);
+    }
+  }
+
+  Future<bool> _onBackPressAppBar() async {
+    SystemNavigator.pop();
+    print('Backpress');
+    return Future.value(false);
+  }
+
+  bool _isEmailValid(String email) {
+    return RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(email);
+  }
+}
---
+++ food_ninja/lib/mainlayout.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+class MainLayout extends StatelessWidget {
+  final String status;
+  MainLayout({Key key, @required this.status}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Food Ninja - Main',
+      home: MyStatefulWidget(),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  MyStatefulWidget({Key key}) : super(key: key);
+
+  @override
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Business',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Ninja Food'),
+        backgroundColor: Colors.red,
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.fastfood),
+            title: Text('Restaurants'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            title: Text('Order Status'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            title: Text('Profile'),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+  
+}
